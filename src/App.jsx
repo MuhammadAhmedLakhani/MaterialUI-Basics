530f6f016509656b12d89ee2f5cef2770a8a8b59
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-22 14:52:58 +0500

    MaterialUI:using own classs to override default button style

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+    <h1>Material UI Practice</h1>
+    <Button className='my-btn'  variant="contained">Contained</Button>
+    </div>
+  );
+}
+
+export default App;
